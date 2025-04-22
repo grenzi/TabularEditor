6aec62d16c72af0964a3c3ad262f91d5d6a3530c
Gage Renzi <gage.renzi@healthstream.com>
2025-04-22 17:34:09 -0500

    Fix missing resource error due to TabularEditor.TOMWrapper versus TabularEditor namespace in this alt build

--- TOMWrapper/Messages.Designer.cs
+++ TOMWrapper/Messages.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TabularEditor {
+namespace TabularEditor.TOMWrapper {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TabularEditor {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TabularEditor.Messages", typeof(Messages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TabularEditor.TOMWrapper.Messages", typeof(Messages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- TOMWrapper/TOMWrapper/MetadataObjectHelper.cs
+++ TOMWrapper/TOMWrapper/MetadataObjectHelper.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TabularEditor;
+using TabularEditor.TOMWrapper;
 
 namespace Microsoft.AnalysisServices.Tabular.Helper
 {
